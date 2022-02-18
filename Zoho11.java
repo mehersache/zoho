1fbe724f5883fbe670cbde4822723de189c37294
mehersache <94534686+mehersache@users.noreply.github.com>
2022-02-18 10:45:30 +0530

    Add files via upload

---
+++ Zoho11.java
@@ -0,0 +1,16 @@
+package zoho;
+
+public class Zoho11 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     String str="WATER";
+     
+    System.out.println("        "+str.charAt(2));
+	System.out.println("       "+str.substring(2,4));
+    System.out.println("      "+str.substring(2,5));
+    System.out.println("     "+str.substring(2,5)+str.charAt(0));
+    System.out.println("    "+str.substring(2,5)+str.substring(0,2));
+     
+}
+}
